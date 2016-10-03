2e917150fd066897de674427203db0a807b5bda7
Jiri Spac <capajj@gmail.com>
2016-10-04 01:54:54 +0200

    let's throw when we are given location outside the file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = (source, sourceLocation) => {
+  if (!source) {
+    throw new TypeError('source parameter is falsy')
+  }
   source = source.replace(new RegExp('\r?\n','g'), '\n')
   function nthIndexOf (pattern, n) {
     let i = -1
@@ -10,8 +13,15 @@ module.exports = (source, sourceLocation) => {
 
     return i
   }
-  start = nthIndexOf('\n', sourceLocation.start.line - 1) + sourceLocation.start.column + 1
-  end = nthIndexOf('\n', sourceLocation.end.line - 1) + sourceLocation.end.column + 1
-
+  const startLineIndex = nthIndexOf('\n', sourceLocation.start.line - 1)
+  const endLineIndex = nthIndexOf('\n', sourceLocation.end.line - 1)
+  if (startLineIndex === -1 && endLineIndex === -1) {
+    throw new Error('source location is outside the file')
+  }
+  start = startLineIndex + sourceLocation.start.column + 1
+  end = endLineIndex + sourceLocation.end.column + 1
+  if (start > source.length || end > source.length) {
+    throw new Error('source location is outside the file')
+  }
   return source.substring(start, end)
 }
--- test/get-source-code-location.spec.js
+++ test/get-source-code-location.spec.js
@@ -1,7 +1,6 @@
 import test from 'ava'
 import getSourceCode from '../index'
 
-
 const sourceLocation = {
  start: { line: 8, column: 2 },
  end: { line: 8, column: 12 }
@@ -21,4 +20,19 @@ test('work with unix line endings', t => {
 
 test('works with windows line endings', t => {
   t.is(getSourceCode(`function foo() {\r\n  var a = 10\r\n  a = 5\r\n  a++\r\n  a--\r\n  --a\r\n  ++a\r\n  return a+1\r\n}`, sourceLocation), 'return a+1')
+})
+
+test('throws if parameters are empty or null', (t) => {
+  t.throws(() => {
+    getSourceCode('', null)
+  }, 'source parameter is falsy')
+})
+
+test('throws if location is outside the file', (t) => {
+  t.throws(() => {
+    getSourceCode('function(){}', {
+     start: { line: 8, column: 2 },
+     end: { line: 8, column: 12 }
+    })
+  }, 'source location is outside the file')
 })
